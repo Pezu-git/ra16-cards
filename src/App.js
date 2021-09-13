feb96c6187da1657b6798dcd0c6b83848a8d577c
Pezu-git <artem_nikolaev69@mail.ru>
2021-09-13 15:20:55 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -5,19 +5,22 @@ import CardText from "./components/CardText";
 import CardTitle from "./components/CardTitle";
 import Button from "./components/Button";
 
-const URL = "https://avatars.mds.yandex.net/get-kinopoisk-image/1946459/89c43dde-6bb9-4c1d-8e2c-59c3af5430d6/800x800"
+const URL = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT5wlE7RxsYhCgvesbEcOzNGgF6nNDsbyxmggq-hc4AeSZnV_sBkgsCcC_2SQal3ysWBrY&usqp=CAU"
 
 export default function App() {
   return (
     <div className='App'>
       <Card imgURL={URL}>
-        <CardTitle>Card Title 1</CardTitle>
-        <CardText>Card Text Card Text Card Text Card Text Card Text</CardText>
+        <CardTitle>JavaScript-библиотека для создания пользовательских интерфейсов</CardTitle>
+        <CardText>Создавать интерактивные пользовательские интерфейсы на React — приятно и просто. 
+          Вам достаточно описать, как части интерфейса приложения выглядят в разных состояниях. 
+          React будет своевременно их обновлять, когда данные изменяются.</CardText>
         <Button>Button 1</Button>
       </Card>
       <Card>
-        <CardTitle>Card Title 2</CardTitle>
-        <CardText>Card Text Card Text Card Text Card Text Card Text</CardText>
+        <CardTitle>Основан на компонентах</CardTitle>
+        <CardText>Создавайте инкапсулированные компоненты с собственным состоянием, 
+          а затем объединяйте их в сложные пользовательские интерфейсы.</CardText>
         <Button>Button 2</Button>
       </Card>
     </div>
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,7 +1,7 @@
 import CardImg from "./CardImg";
 
 
-const URL = "https://avatars.mds.yandex.net/get-kinopoisk-image/1946459/89c43dde-6bb9-4c1d-8e2c-59c3af5430d6/800x800"
+const URL = "https://www.google.com/url?sa=i&url=https%3A%2F%2Fms314006.github.io%2Fsimple-implementing-ssr-in-react%2F&psig=AOvVaw3IndgCJ5FOh77zA0k6iVMT&ust=1631621851015000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCPipxLv2-_ICFQAAAAAdAAAAABAD"
 
 const Card = (props) => (
     <div className="container">
